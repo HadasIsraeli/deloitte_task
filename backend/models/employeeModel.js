935a5434a55839716821690455eddd56e6c7e638
HADASISRAELI\hadas <hadas25496@gmail.com>
2023-05-23 20:02:40 +0300

    Employee details page

--- backend/models/employeeModel.js
+++ backend/models/employeeModel.js
@@ -5,6 +5,9 @@ const EmployeeSchema = new Schema({
     Name: { type: String, required: true },
     WorkTitle: { type: String, required: true },
     Avatar: { type: String, required: true },
+    Address: { type: String, required: true },
+    Food: { type: String, required: true },
+    Hobby: { type: String, required: true },
 }, { timestamps: true })
 
 module.exports = mongoose.model('Employee', EmployeeSchema);
--- src/components/EmployeeDetails.js
+++ src/components/EmployeeDetails.js
@@ -13,11 +13,15 @@ const EmployeeDetails = () => {
             {error && <div><i class="fa-solid fa-triangle-exclamation fa-beat"></i> Error! {error} trying again...</div>}
             {employee &&
                 <div className="employee-res" key={employee._id}>
-                    <div>
+                    <div className="name-avater">
                         <i class={employee.Avatar}></i>
                         <h4>{employee.Name}</h4>
                     </div>
-                    <p>{employee.WorkTitle}</p>
+                    <p><i class="fa-solid fa-chalkboard-user"></i> {employee.WorkTitle}</p>
+                    <p><i class="fa-solid fa-house-user"></i> {employee.Address}</p>
+                    <p><i class="fa-solid fa-utensils"></i> {employee.Food}</p>
+                    <p><i class="fa-solid fa-heart"></i> {employee.Hobby}</p>
+
                 </div>
             }
         </div>
