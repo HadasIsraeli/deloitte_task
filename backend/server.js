c6e85b988a4eb1dd872bfd9e07dbb0bf5cccee21
HADASISRAELI\hadas <hadas25496@gmail.com>
2023-05-29 12:10:21 +0300

    small changes for deploying render netlify

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const employeeRoutes = require('./routes/employee');
+const cors = require('cors');
 
 const app = express();
 
