97f43d51869897621e720501c5f0cd8d3466273a
HADASISRAELI\hadas <hadas25496@gmail.com>
2023-05-29 13:13:33 +0300

    small changes for deploying render netlify back to what it was before

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -2,48 +2,11 @@ const express = require('express');
 const { getEmployees, getEmployee, getSearch } = require('../controllers/employeesController');
 const router = express.Router();
 
-const allowedOrigins = [
-    "http://localhost:3000",
-    "https://task-deloitte.netlify.app"
-  ];
 
-router.get('/employees', (req, res) => {
-  const requestOrigin = req.headers.origin;
+router.get('/employees', getEmployees);
 
-  if (allowedOrigins.includes(requestOrigin)) {
-    res.header('Access-Control-Allow-Origin', requestOrigin);
-  }
+router.post('/search', getSearch);
 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-  getEmployees(req, res);
-});
-
-router.post('/search', (req, res) => {
-    const requestOrigin = req.headers.origin;
-  
-    if (allowedOrigins.includes(requestOrigin)) {
-      res.header('Access-Control-Allow-Origin', requestOrigin);
-    }
-  
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-  
-    getSearch(req, res);
-  });
-
-router.get('/employee/:id',(req, res) => {
-    const requestOrigin = req.headers.origin;
-  
-    if (allowedOrigins.includes(requestOrigin)) {
-      res.header('Access-Control-Allow-Origin', requestOrigin);
-    }
-    
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-  
-    getEmployee(req, res);
-  });
+router.get('/employee/:id', getEmployee);
 
 module.exports = router;
